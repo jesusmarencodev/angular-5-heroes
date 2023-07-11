83af576f6433e0a74065e28e740b52f8db40be1b
Jesus Marenco <jesusmarencodev@gmail.com>
2023-07-11 16:14:35 -0500

    guard authenticated

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
 
 @Component({
   selector: 'app-layout-page',
@@ -13,5 +16,15 @@ export class LayoutPageComponent {
     {label:'Add', icon:'add', url:'./new-hero'},
     {label:'Search', icon:'search', url:'./search'},
   ]
+  constructor(private authService: AuthService, private router: Router) {}
+
+  get user():User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout():void{
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 
 }
